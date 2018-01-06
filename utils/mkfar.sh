88cac3516176f8dd847daafe25499aeaac33e3c4
Stas Sergeev <stsp@users.sourceforge.net>
2018-01-07 02:35:40 +0300

    macrosifier fix

---
+++ utils/mkfar.sh
@@ -0,0 +1,7 @@
+sed -e ':loop' \
+	-e "s/\([^(, ]\+\) $1 *\*\(\**\)/$2(\1\2)/" \
+	-e "s/struct $2(/$2(struct /" \
+	-e "s/const $2(/$2(const /" \
+	-e "s/CONST $2(/$2(CONST /" \
+	-e "s/unsigned $2(/$2(unsigned /" \
+	-e 't loop' $3
