7093d0e7b50a039576ed664663c6387e62aace66
Stas Sergeev <stsp@users.sourceforge.net>
2018-04-15 04:28:48 +0300

    farobj: fix string length
    Forgotten space for \0.

--- hdr/farobj.hpp
+++ hdr/farobj.hpp
@@ -115,7 +115,7 @@ public:
     __obj_##n.FarObjSet(o)
 #define _MK_FAR_STR_ST(n, o) \
     static FarObjSt<_R(o)> __obj_##n; \
-    __obj_##n.FarObjSet(o, strlen(o))
+    __obj_##n.FarObjSet(o, strlen(o) + 1)
 #define MK_FAR(o) \
         FarPtr<decltype(o)>(std::allocate_shared<FarObj<decltype(o)>>\
         (Mallocator<FarObj<decltype(o)>>(), o))
@@ -129,10 +129,7 @@ public:
 #define MK_FAR_PTR_SCP(o) FarPtr<_R(o)>(FarObj<_R(o)>(*o).get_obj())
 #define __MK_FAR(n) FarPtr<decltype(__obj_##n)::obj_type>(__obj_##n.get_obj())
 #define __MK_NEAR(n) __obj_##n.get_near()
-#define _MK_FAR_STR(n, o) FarObj<_R(o)> __obj_##n(o, strlen(o))
-#define _MK_FAR_STR_ST(n, o) \
-    static FarObjSt<_R(o)> __obj_##n; \
-    __obj_##n.FarObjSet(o, strlen(o))
+#define _MK_FAR_STR(n, o) FarObj<_R(o)> __obj_##n(o, strlen(o) + 1)
 #define MK_FAR_STR_ST(o) ({ _MK_FAR_STR_ST(_ddd, o); __MK_FAR(_ddd); })
 #define _MK_FAR_SZ(n, o, sz) FarObj<_R(o)> __obj_##n(o, sz)
 #define _MK_FAR_SZ_ST(n, o, sz) \
@@ -141,4 +138,4 @@ public:
 #define MK_FAR_SZ_ST(o, sz) ({ _MK_FAR_SZ_ST(_ddd, o, sz); __MK_FAR(_ddd); })
 #define MK_FAR_SCP(o) FarPtr<decltype(o)>(FarObj<decltype(o)>(o).get_obj())
 #define MK_FAR_PTR_SCP(o) FarPtr<_R(o)>(FarObj<_R(o)>(*o).get_obj())
-#define MK_FAR_STR_SCP(o) FarPtr<_R(o)>(FarObj<_R(o)>(o, strlen(o)).get_obj())
+#define MK_FAR_STR_SCP(o) FarPtr<_R(o)>(FarObj<_R(o)>(o, strlen(o) + 1).get_obj())
