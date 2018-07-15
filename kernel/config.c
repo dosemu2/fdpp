dosemu2/fdpp

commit e6f026891ef3413ffcd847c171590463f65c15e2

fix all gcc warnings

kernel/config.c
@@ -217,7 +217,8 @@ STATIC BOOL isnum(char ch);
 #if 0
 STATIC COUNT tolower(COUNT c);
 #endif
-STATIC char toupper(char c);
+//STATIC char toupper(char c);
+#define toupper(c)      ((c) >= 'a' && (c) <= 'z' ? (c) + ('A' - 'a') : (c))
 STATIC VOID strupr(char *s);
 STATIC VOID mcb_init(UCOUNT seg, UWORD size, BYTE type);
 STATIC VOID mumcb_init(UCOUNT seg, UWORD size);
@@ -1552,7 +1553,7 @@ err:printf("%s has invalid format\n", filename);
   {
     if (read(fd, &entry, sizeof(entry)) != sizeof(entry) || entry.length != 12)
       goto err;
-    if (entry.country != ctryCode || entry.codepage != codePage && codePage)
+    if (entry.country != ctryCode || (entry.codepage != codePage && codePage))
       continue;
     if (lseek(fd, entry.offset) == 0xffffffffL
       || read(fd, &count, sizeof(count)) != sizeof(count)
@@ -1568,17 +1569,17 @@ err:printf("%s has invalid format\n", filename);
         continue;
       if (lseek(fd, hdr[i].offset) == 0xffffffffL
        || read(fd, &subf_data, 10) != 10
-       || memcmp(subf_data.signature, table[hdr[i].id].sig, 8) && (hdr[i].id !=4
-       || memcmp(subf_data.signature, table[2].sig, 8))  /* UCASE for FUCASE ^*/
+       || (memcmp(subf_data.signature, table[hdr[i].id].sig, 8) && (hdr[i].id !=4
+       || memcmp(subf_data.signature, table[2].sig, 8)))  /* UCASE for FUCASE ^*/
        || read(fd, subf_data.buffer, subf_data.length) != subf_data.length)
         goto err;
       if (hdr[i].id == 1)
       {
         if (((struct CountrySpecificInfo *)subf_data.buffer)->CountryID
                                                      != entry.country
-         || ((struct CountrySpecificInfo *)subf_data.buffer)->CodePage
+         || (((struct CountrySpecificInfo *)subf_data.buffer)->CodePage
                                                      != entry.codepage
-         && codePage)
+         && codePage))
           continue;
         nlsPackageHardcoded.cntry = entry.country;
         nlsPackageHardcoded.cp = entry.codepage;
@@ -2021,6 +2022,7 @@ STATIC BOOL isnum(char ch)
   return (ch >= '0' && ch <= '9');
 }
 
+#if 0
 /* Yet another change for true portability (PJV) */
 STATIC char toupper(char c)
 {
@@ -2028,6 +2030,7 @@ STATIC char toupper(char c)
     c -= 'a' - 'A';
   return c;
 }
+#endif
 
 /* Convert string s to uppercase */
 STATIC VOID strupr(char *s)
@@ -2661,7 +2664,7 @@ STATIC VOID InstallExec(struct instCmds *icmd)
   }
 }
 
-STATIC void free(seg segment)
+STATIC void _free(seg segment)
 {
   iregs r;
 
@@ -2698,7 +2701,7 @@ VOID DoInstall(void)
   */
 
   set_strategy(LAST_FIT);
-  installMemory = ((unsigned)_init_end + ebda_size + 15) / 16;
+  installMemory = ((uintptr_t)_init_end + ebda_size + 15) / 16;
 #ifdef __WATCOMC__
   installMemory += (_InitTextEnd - _InitTextStart + 15) / 16;
 #endif
@@ -2713,7 +2716,7 @@ VOID DoInstall(void)
     InstallExec(cmd);
   }
   set_strategy(FIRST_FIT);
-  free(installMemory);
+  _free(installMemory);
 
   InstallPrintf(("Done with installing commands\n"));
   return;

hdr/portab.h
@@ -58,7 +58,40 @@ static char *portab_hRcsId =
 /*                                                              */
 /****************************************************************/
 
-                                                        /* commandline overflow - removing -DI86 TE */
+/* Types for `void *' pointers.  */
+#ifdef __x86_64__
+# ifndef __intptr_t_defined
+typedef long int                intptr_t;
+#  define __intptr_t_defined
+# endif
+typedef unsigned long int       uintptr_t;
+#else
+# ifndef __intptr_t_defined
+typedef int                     intptr_t;
+#  define __intptr_t_defined
+# endif
+typedef unsigned int            uintptr_t;
+#endif
+
+/*                                                              */
+/* Common byte, 16 bit and 32 bit types                         */
+/*                                                              */
+typedef char BYTE;
+typedef short WORD;
+typedef long DWORD;
+
+typedef unsigned char UBYTE;
+typedef unsigned short UWORD;
+typedef unsigned long UDWORD;
+
+typedef short SHORT;
+
+typedef unsigned int BITS;      /* for use in bit fields(!)     */
+
+typedef int COUNT;
+typedef unsigned int UCOUNT;
+typedef unsigned long ULONG;
+
 #if defined(__TURBOC__)
 
 #define I86
@@ -141,8 +174,22 @@ unsigned short getSS(void);
 
 #elif defined(__GNUC__)
 #define VA_CDECL
+UWORD getCS(void);
 #define _CS getCS()
+UWORD getSS(void);
 #define _SS getSS()
+UBYTE peekb(UWORD seg, UWORD ofs);
+UWORD peekw(UWORD seg, UWORD ofs);
+ULONG peekl(UWORD seg, UWORD ofs);
+void pokeb(UWORD seg, UWORD ofs, UBYTE b);
+void pokew(UWORD seg, UWORD ofs, UWORD w);
+void pokel(UWORD seg, UWORD ofs, ULONG l);
+void *short_ptr(UWORD offs);
+#define MK_SP(offs) short_ptr(offs)
+UWORD mk_offs(UBYTE *data, UWORD len);
+#define MK_OFFS(data) mk_offs(data, sizeof(data))
+void disable(void);
+void enable(void);
 /* for warnings only ! */
 #define MC68K
 
@@ -226,25 +273,6 @@ typedef int BOOL;
 #ifndef min
 #define min(a,b)       (((a) < (b)) ? (a) : (b))
 #endif
-
-/*                                                              */
-/* Common byte, 16 bit and 32 bit types                         */
-/*                                                              */
-typedef char BYTE;
-typedef short WORD;
-typedef long DWORD;
-
-typedef unsigned char UBYTE;
-typedef unsigned short UWORD;
-typedef unsigned long UDWORD;
-
-typedef short SHORT;
-
-typedef unsigned int BITS;      /* for use in bit fields(!)     */
-
-typedef int COUNT;
-typedef unsigned int UCOUNT;
-typedef unsigned long ULONG;
 
 #ifdef WITHFAT32
 typedef unsigned long CLUSTER;

kernel/dosfns.c
@@ -550,7 +550,7 @@ long DosOpenSft(char FAR * fname, unsigned flags, unsigned attrib)
       cmd = REM_OPEN;
       attrib = (BYTE)flags;
     }
-    status = (int)network_redirector_mx(cmd, sftp, (void *)attrib);
+    status = (int)network_redirector_mx(cmd, sftp, MK_SP(attrib));
     if (status >= SUCCESS)
     {
       if (sftp->sft_count == 0)

kernel/initclk.c
@@ -45,8 +45,8 @@ STATIC int InitBcdToByte(int x)
 
 void Init_clk_driver(void)
 {
-  static iregs regsT = {0x200}; /* ah=0x02 */
-  static iregs regsD = {0x400, 0, 0x1400, 0x101};
+  static iregs regsT = {{0x200}}; /* ah=0x02 */
+  static iregs regsD = {{0x400}, {0}, {0x1400}, {0x101}};
                       /* ah=4, ch=20^ ^cl=0, ^dh=dl=1 (2000/1/1)
                        * (above date will be set on error) */
   iregs dosregs;

kernel/initdisk.c
@@ -725,7 +725,7 @@ StandardBios:                  /* old way to get parameters */
   driveParam->chs.Head = (regs.d.x >> 8) + 1; /* DH = max head value = # of heads - 1 (0-255) */
   driveParam->chs.Sector = (regs.c.x & 0x3f); /* CL bits 0-5 = max sector value = # (sectors/track) - 1 (1-63) */
   /* max cylinder value = # cylinders - 1 (0-1023) = [high two bits]CL7:6=cyls9:8, [low byte]CH=cyls7:0 */
-  driveParam->chs.Cylinder = (regs.c.x >> 8) | ((regs.c.x & 0xc0) << 2) + 1;
+  driveParam->chs.Cylinder = ((regs.c.x >> 8) | ((regs.c.x & 0xc0) << 2)) + 1;
 
   if (driveParam->chs.Sector == 0) {
     /* happens e.g. with Bochs 1.x if no harddisk defined */
@@ -804,9 +804,9 @@ BOOL is_suspect(struct CHS *chs, struct CHS *pEntry_chs)
   return !((pEntry_chs->Cylinder == chs->Cylinder &&
             pEntry_chs->Head     == chs->Head     &&
             pEntry_chs->Sector   == chs->Sector)        ||
-           chs->Cylinder > 1023u &&
+           (chs->Cylinder > 1023u &&
            (pEntry_chs->Cylinder == 1023 ||
-            pEntry_chs->Cylinder == (0x3ff & chs->Cylinder)));
+            pEntry_chs->Cylinder == (0x3ff & chs->Cylinder))));
 }
 
 void print_warning_suspect(char *partitionName, UBYTE fs, struct CHS *chs,

kernel/initoem.c
@@ -47,19 +47,19 @@ unsigned init_oem(void)
 
 void movebda(size_t bytes, unsigned new_seg)
 {
-  unsigned old_seg = peek(0, EBDASEG);
+  unsigned old_seg = peekw(0, EBDASEG);
   fmemcpy(MK_FP(new_seg, 0), MK_FP(old_seg, 0), bytes);
-  poke(0, EBDASEG, new_seg);
-  poke(0, RAMSIZE, ram_top);
+  pokew(0, EBDASEG, new_seg);
+  pokew(0, RAMSIZE, ram_top);
 }
 
 unsigned ebdasize(void)
 {
-  unsigned ebdaseg = peek(0, EBDASEG);
+  unsigned ebdaseg = peekw(0, EBDASEG);
   unsigned ramsize = ram_top;
 
-  if (ramsize == peek(0, RAMSIZE))
-    if (ramsize * 64 == ebdaseg && ramsize < 640 && peek(0, RAMSIZE) == ramsize)
+  if (ramsize == peekw(0, RAMSIZE))
+    if (ramsize * 64 == ebdaseg && ramsize < 640 && peekw(0, RAMSIZE) == ramsize)
     {
     unsigned ebdasz = peekb(ebdaseg, 0);
 

kernel/inthndlr.c
@@ -1294,7 +1294,7 @@ dispatch:
           break;
 
         default:
-          rc = (int)network_redirector_mx(REM_PRINTSET, &lr, (void *)Int21AX);
+          rc = (int)network_redirector_mx(REM_PRINTSET, &lr, MK_SP(Int21AX));
           goto short_check;
       }
       break;
@@ -1318,7 +1318,7 @@ dispatch:
       }
       else
       {
-        rc = (int)network_redirector_mx(REM_DOREDIRECT, &lr, (void *)Int21AX);
+        rc = (int)network_redirector_mx(REM_DOREDIRECT, &lr, MK_SP(Int21AX));
         /* the remote function manipulates *r directly !,
            so we should not copy lr to r here            */
         if (rc != SUCCESS)

kernel/main.c
@@ -701,13 +701,13 @@ STATIC VOID InitSerialPorts(VOID)
 STATIC int EmulatedDriveStatus(int drive,char statusOnly)
 {
   iregs r;
-  char buffer[0x13];
+  UBYTE buffer[0x13];
   buffer[0] = 0x13;
 
   r.a.b.h = 0x4b;               /* bootable CDROM - get status */
   r.a.b.l = statusOnly;
   r.d.b.l = (char)drive;
-  r.si  = (int)buffer;
+  r.si  = MK_OFFS(buffer);
   init_call_intr(0x13, &r);
 
   if (r.flags & 1)

commit 0af604a22f08275b9b72f2cc045c2b043e990a34

config: remove horrible toupper() macro
The bug was introduced by e6f02689.

kernel/config.c
@@ -221,8 +221,9 @@ STATIC BOOL isnum(char ch);
 #if 0
 STATIC COUNT tolower(COUNT c);
 #endif
-//STATIC char toupper(char c);
-#define toupper(c)      ((c) >= 'a' && (c) <= 'z' ? (c) + ('A' - 'a') : (c))
+#ifndef USE_STDLIB
+STATIC char toupper(const char c);
+#endif
 STATIC VOID strupr(char *s);
 STATIC VOID mcb_init(UCOUNT seg, UWORD size, BYTE type);
 STATIC VOID mumcb_init(UCOUNT seg, UWORD size);
@@ -2026,7 +2027,7 @@ STATIC BOOL isnum(char ch)
   return (ch >= '0' && ch <= '9');
 }
 
-#if 0
+#ifndef USE_STDLIB
 /* Yet another change for true portability (PJV) */
 STATIC char toupper(char c)
 {