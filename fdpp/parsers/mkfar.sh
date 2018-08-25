ea54e4c4880805d54ab3688124aa454f9110832b
Stas Sergeev <stsp@users.sourceforge.net>
2018-08-26 00:05:46 +0300

    move hardcoded FAR names to mkfar.sh

--- fdpp/parsers/mkfar.sh
+++ fdpp/parsers/mkfar.sh
@@ -1,7 +1,11 @@
-sed -e ':loop' \
+mkfar() {
+    sed -e ':loop' \
 	-e "s/\([^(, ]\+\) $1 *\*/$2(\1)/" \
 	-e "s/struct $2(/$2(struct /" \
 	-e "s/const $2(/$2(const /" \
 	-e "s/CONST $2(/$2(CONST /" \
 	-e "s/unsigned $2(/$2(unsigned /" \
 	-e 't loop' $3
+}
+
+mkfar FAR __FAR $1
