892a76a953ca931aef54f21adf41abf55d4c7784
Stas Sergeev <stsp@users.sourceforge.net>
2018-08-27 22:03:20 +0300

    parallel build fix

--- fdpp/parsers/parse_decls.sh
+++ fdpp/parsers/parse_decls.sh
@@ -4,30 +4,41 @@ set -e
 set -o pipefail
 
 gen_calls_tmp() {
-	grep ASMCFUNC $1 | cpp -P -I $3 -include unfar.h | nl -n ln -v 0 >$2
+	grep ASMCFUNC $1 | cpp -P -I $srcdir -include unfar.h | nl -n ln -v 0
 }
 
 gen_plt_inc() {
-	grep ") FAR " $1 | sed 's/\([0-9]\+\).\+ \(SEGM(\(.\+\))\).* \([^ (]\+\) *(.*/asmcfunc_f \4,\t\t\1,\t\3/' >$2
-	grep -v ") FAR " $1 | sed 's/\([0-9]\+\).\+ \(SEGM(\(.\+\))\).* \([^ (]\+\) *(.*/asmcfunc_n \4,\t\t\1,\t\3/' >>$2
+	grep ") FAR " $1 | sed 's/\([0-9]\+\).\+ \(SEGM(\(.\+\))\).* \([^ (]\+\) *(.*/asmcfunc_f \4,\t\t\1,\t\3/'
+	grep -v ") FAR " $1 | sed 's/\([0-9]\+\).\+ \(SEGM(\(.\+\))\).* \([^ (]\+\) *(.*/asmcfunc_n \4,\t\t\1,\t\3/'
 }
 
 gen_asms_tmp() {
 	grep 'ASMFUNC\|ASMPASCAL' $1 | grep -v "//" | \
-		cpp -P -I $3 -include unfar.h | nl -n ln -v 0 >$2
+		cpp -P -I $srcdir -include unfar.h | nl -n ln -v 0
 }
 
 gen_plt_asmc() {
-	grep ASMFUNC $1 | sed 's/\([0-9]\+\).\+ \([^ (]\+\) *(.\+/asmcsym \2,\t\t\1/' >$2
+	grep ASMFUNC $1 | sed 's/\([0-9]\+\).\+ \([^ (]\+\) *(.\+/asmcsym \2,\t\t\1/'
 }
 
 gen_plt_asmp() {
-	grep ASMPASCAL $1 | sed 's/\([0-9]\+\).\+ \([^ (]\+\) *(.\+/asmpsym \U\2,\t\t\1/' >$2
+	grep ASMPASCAL $1 | sed 's/\([0-9]\+\).\+ \([^ (]\+\) *(.\+/asmpsym \U\2,\t\t\1/'
 }
 
-
-gen_calls_tmp $1 $5 $7
-gen_plt_inc $5 $2
-gen_asms_tmp $1 $6 $7
-gen_plt_asmc $6 $3
-gen_plt_asmp $6 $4
+case "$1" in
+1)
+	gen_calls_tmp $2
+	;;
+2)
+	gen_asms_tmp $2
+	;;
+3)
+	gen_plt_inc $2
+	;;
+4)
+	gen_plt_asmc $2
+	;;
+5)
+	gen_plt_asmp $2
+	;;
+esac
