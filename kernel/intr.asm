64fa465dd262e5490687814622f75f8b998538bb
Stas Sergeev <stsp@users.sourceforge.net>
2019-06-17 04:17:14 +0300

    intr: re-macrosify INTR [#25]

--- kernel/intr.asm
+++ kernel/intr.asm
@@ -27,57 +27,59 @@
 
 		%include "segs.inc"
 
-%macro INTR 0
+%macro INTR 3
+        push bp                      ; Standard C entry
+        mov  bp,sp
+        push si
+        push di
+        push ds
+        pushf
 
-                push    bp                      ; Standard C entry
-                mov     bp,sp
-                push    si
-                push    di
-                push	ds
-                pushf
+        mov ax, [bp+%1]               ; interrupt number
+        mov [cs:.intr_1-1], al
+        jmp short .intr_2           ; flush the instruction cache
+.intr_2
+        mov bx, [bp+4]               ; regpack structure
+%if %3 == 1
+        mov ds, [bp+6]
+%endif
+        mov ax, [bx]
+        mov cx, [bx+4]
+        mov dx, [bx+6]
+        mov si, [bx+8]
+        mov di, [bx+10]
+;        mov bp, [bx+12]
+        push word [bx+14]            ; ds
+        mov es, [bx+16]
+        push word [bx+22]            ; flags
+        popf
+        mov bx, [bx+2]
+        pop ds
+        int 0
+.intr_1:
 
-                mov	ax, [bp+6]		; interrupt number
-                mov	[cs:%%intr_1-1], al
-                jmp 	short %%intr_2		; flush the instruction cache
-%%intr_2	mov	bx, [bp+4]		; regpack structure
-		mov	ax, [bx]
-		mov	cx, [bx+4]
-		mov	dx, [bx+6]
-		mov	si, [bx+8]
-		mov	di, [bx+10]
-;		mov	bp, [bx+12]
-		push	word [bx+14]		; ds
-		mov	es, [bx+16]
-		push 	word [bx+22]		; flags
-		popf
-		mov	bx, [bx+2]
-		pop	ds
-		int	0
-%%intr_1:
+        pushf
+        push ds
+        push bx
+        mov bx, sp
+        mov bx, [bp+4]               ; address of REGPACK
+        mov ds, [bp%2]
+        mov [bx], ax
+        pop word [bx+2]              ; bx
+        mov [bx+4], cx
+        mov [bx+6], dx
+        mov [bx+8], si
+        mov [bx+10], di
+;        mov [bx+12], bp
+        pop word [bx+14]             ; ds
+        mov [bx+16], es
+        pop word [bx+22]             ; flags
 
-		pushf
-		push	ds
-		push	bx
-		mov	bx, sp
-		mov	bx, [bp+4]		; address of REGPACK
-		mov	ds, [bp-6]
-		mov	[bx], ax
-		pop	word [bx+2]		; bx
-		mov	[bx+4], cx
-		mov	[bx+6], dx
-		mov	[bx+8], si
-		mov	[bx+10], di
-;		mov	[bx+12], bp
-		pop	word [bx+14]		; ds
-		mov	[bx+16], es
-		pop	word [bx+22]		; flags
-
-		popf
-		pop	ds
-		pop	di
-		pop	si
-		pop	bp
-		ret     4
+        popf
+        pop ds
+        pop di
+        pop si
+        pop bp
 %endmacro
 
 segment HMA_TEXT
@@ -125,56 +127,7 @@ no_read_error:
 ;
     global CALL_INTR
 CALL_INTR:
-        push bp                      ; Standard C entry
-        mov  bp,sp
-        push si
-        push di
-        push ds
-        pushf
-
-        mov ax, [bp+8]               ; interrupt number
-        mov [cs:.intr_1-1], al
-        jmp short .intr_2           ; flush the instruction cache
-.intr_2
-        mov bx, [bp+4]               ; regpack structure
-        mov ds, [bp+6]
-        mov ax, [bx]
-        mov cx, [bx+4]
-        mov dx, [bx+6]
-        mov si, [bx+8]
-        mov di, [bx+10]
-;        mov bp, [bx+12]
-        push word [bx+14]            ; ds
-        mov es, [bx+16]
-        push word [bx+22]            ; flags
-        popf
-        mov bx, [bx+2]
-        pop ds
-        int 0
-.intr_1:
-
-        pushf
-        push ds
-        push bx
-        mov bx, sp
-        mov bx, [bp+4]               ; address of REGPACK
-        mov ds, [bp+6]
-        mov [bx], ax
-        pop word [bx+2]              ; bx
-        mov [bx+4], cx
-        mov [bx+6], dx
-        mov [bx+8], si
-        mov [bx+10], di
-;        mov [bx+12], bp
-        pop word [bx+14]             ; ds
-        mov [bx+16], es
-        pop word [bx+22]             ; flags
-
-        popf
-        pop ds
-        pop di
-        pop si
-        pop bp
+        INTR 8,+6,1
         ret 6
 
 ;
@@ -237,7 +190,8 @@ segment	INIT_TEXT
 ;
 		global	INIT_CALL_INTR
 INIT_CALL_INTR:
-		INTR
+		INTR 6,-6,0
+		ret 4
 
 ;
 ; int init_call_XMScall( (WORD FAR * driverAddress)(), WORD AX, WORD DX)
