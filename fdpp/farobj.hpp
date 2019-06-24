dosemu2/fdpp

commit d3a7a357989259aad4416b51ab2504cc655a78d2

implement call_intr() for console output [fixes #22]
Using init_call_intr() was causing the weird undebuggable
crashes because the INIT_TEXT code can be overwritten.

kernel/chario.c
@@ -158,7 +158,7 @@ STATIC void fast_put_char(unsigned char chr)
 #else
   iregs r = {};
   r.a.b.l = chr;
-  init_call_intr(0x29, &r);
+  call_intr(0x29, &r);
 #endif
 }
 #endif

kernel/prf.c
@@ -116,7 +116,7 @@ void put_console(int c)
 #else
   iregs r = {};
   r.a.b.l = c;
-  init_call_intr(0x29, &r);
+  call_intr(0x29, &r);
 #endif
 }
 #endif                          /*  DOSEMU   */

kernel/intr.asm
@@ -125,6 +125,14 @@ RES_READ:
         mov ax, -1
 no_read_error:
         ret
+;
+;       void call_intr(nr, rp)
+;       REG int nr
+;       REG struct REGPACK *rp
+;
+		global	CALL_INTR
+CALL_INTR:
+		INTR
 
 segment	INIT_TEXT
 ;

kernel/proto.h
@@ -494,6 +494,7 @@ COUNT ASMFUNC CriticalError(COUNT nFlag, COUNT nDrive, COUNT nError,__FAR(struct
 VOID ASMFUNC FAR CharMapSrvc(VOID);
 
 UWORD ASMPASCAL init_call_intr(WORD nr, iregs * rp);
+UWORD ASMPASCAL call_intr(WORD nr, iregs * rp);
 UWORD ASMPASCAL read(WORD fd, void *buf, UWORD count);
 WORD ASMPASCAL init_DosOpen(const char *pathname, WORD flags);
 WORD ASMPASCAL close(WORD fd);

commit d4709e93b4426d00ed272ab146ce6365ddc173d2

avoid calling hma_text before it is relocated [#78]
The problem was introduced by d3a7a357.
In principle we can call hma_text code before it is relocated.
But if it is overwritten by dyndata, then we suck.
So how can it be relocated if it is overwritten, you ask.
Well, it can. freedos makes the copy at start. So if the original
is overwritten, the copy can be perfectly used after relocation process.

fdpp/farobj.hpp
@@ -134,7 +134,7 @@ public:
 
 #define _RP(t) typename std::remove_pointer<t>::type
 #define _RE(t) typename std::remove_extent<t>::type
-//#define _RR(t) typename std::remove_reference<t>::type
+#define _RR(t) typename std::remove_reference<t>::type
 #define _R(o) _RP(_RE(_RR(decltype(o))))
 #define _RC(t) typename std::remove_const<_R(t)>::type
 
@@ -163,7 +163,7 @@ public:
     track_owner_sh(&p, &p.m, _sh); \
     p.m = FarPtrBase<_RC(o)> (((FarObj<_R(o)> *)_sh.get())->get_obj()); \
 } while(0)
-#define MK_FAR_SCP(o) FarPtr<decltype(o)>(FarObj<decltype(o)>(o, NM).get_obj())
+#define MK_FAR_SCP(o) FarPtr<_RR(decltype(o))>(FarObj<_RR(decltype(o))>(o, NM).get_obj())
 #define MK_FAR_SZ_SCP(o, sz) FarPtr<_RC(o)>(FarObj<_R(o)>(o, sz, false, NM).get_obj())
 
 #define PTR_MEMB(t) NearPtr_DO<t>

kernel/chario.c
@@ -36,6 +36,7 @@ static BYTE *charioRcsId =
 #endif
 
 #include "globals.h"
+#include "init-mod.h"
 
 STATIC int CharRequest(__DOSFAR(struct dhdr) *pdev, unsigned command)
 {
@@ -158,7 +159,7 @@ STATIC void fast_put_char(unsigned char chr)
 #else
   iregs r = {};
   r.a.b.l = chr;
-  call_intr(0x29, MK_FAR_SCP(r));
+  fd_call_intr(0x29, &r);
 #endif
 }
 #endif

kernel/init-mod.h
@@ -115,6 +115,7 @@ COUNT dsk_init(VOID);
 /* inithma.c */
 int MoveKernelToHMA(void);
 __FAR(VOID) HMAalloc(UCOUNT bytesToAllocate);
+UWORD fd_call_intr(WORD nr, iregs *rp);
 
 /* initoem.c */
 unsigned init_oem(void);

kernel/inithma.c
@@ -363,3 +363,9 @@ void MoveKernel(UWORD NewKernelSegment)
   CurrentKernelSegment = NewKernelSegment;
 }
 
+UWORD fd_call_intr(WORD nr, iregs *rp)
+{
+  if (CurrentKernelSegment)
+    return call_intr(nr, MK_FAR_SCP(*rp));
+  return init_call_intr(nr, rp);
+}

kernel/prf.c
@@ -29,7 +29,7 @@
 #include "portab.h"
 #include "pcb.h"
 #include "globals.h"
-#include "proto.h"
+#include "init-mod.h"
 
 #ifdef FORSYS
 #include <io.h>
@@ -116,7 +116,7 @@ void put_console(int c)
 #else
   iregs r = {};
   r.a.b.l = c;
-  call_intr(0x29, MK_FAR_SCP(r));
+  fd_call_intr(0x29, &r);
 #endif
 }
 #endif                          /*  DOSEMU   */