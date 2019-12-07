886788a95513b0fc4992b21d46d2ecf124accf4f
Stas Sergeev <stsp@users.sourceforge.net>
2019-12-07 22:45:19 +0300

    parsers: move to sed -E syntax
    This is compatible with FreeBSD sed, and is a much saner
    syntax overall.

--- fdpp/parsers/mkfar.sh
+++ fdpp/parsers/mkfar.sh
@@ -1,10 +1,11 @@
 mkfar() {
-    sed -e ':loop' \
-	-e "s/\([^(, ]\+\) $1 *\*/$2(\1)/" \
-	-e "s/struct $2(/$2(struct /" \
-	-e "s/const $2(/$2(const /" \
-	-e "s/CONST $2(/$2(CONST /" \
-	-e "s/unsigned $2(/$2(unsigned /" \
+    sed -E \
+	-e ':loop' \
+	-e "s/([^\(, ]+) $1 *\*/$2\(\1\)/" \
+	-e "s/struct $2\(/$2\(struct /" \
+	-e "s/const $2\(/$2\(const /" \
+	-e "s/CONST $2\(/$2\(CONST /" \
+	-e "s/unsigned $2\(/$2\(unsigned /" \
 	-e 't loop' $3
 }
 
