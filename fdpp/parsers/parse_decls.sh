1b0156d7206ed8cb2223a45ee48ca8e0c3890c67
Stas Sergeev <stsp@users.sourceforge.net>
2019-12-07 23:41:35 +0300

    parse_decls.sh: look for gsed where bsd sed is hopeless

--- fdpp/parsers/parse_decls.sh
+++ fdpp/parsers/parse_decls.sh
@@ -22,7 +22,13 @@ gen_plt_asmc() {
 }
 
 gen_plt_asmp() {
-	grep ASMPASCAL $1 | sed -E 's/([0-9]+).+ ([^ \(]+) *\(.+/asmpsym \U\2, \1/'
+	set +e
+	GSED=`which gsed 2>/dev/null`
+	set -e
+	if [ -z "$GSED" ]; then
+		GSED=`which sed 2>/dev/null`
+	fi
+	grep ASMPASCAL $1 | $GSED -E 's/([0-9]+).+ ([^ \(]+) *\(.+/asmpsym \U\2, \1/'
 }
 
 case "$1" in
