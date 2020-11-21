dosemu2/fdpp

commit d300a36c13068811d1d34523b4195368c72ed4d3

config: fix tokenization [fixes #98]
Parser looks for digits and some special symbols at the
beginning of the line. But by mistake it was also treating
digits and special symbols the same way in the middle of
the line. Maybe ';' (comment sign) can be considered in the
middle of the line, but hope no one is ever doing that.
Certainly we don't want `shell=4dos.com` to become `shell=dos.com`.

kernel/config.c
@@ -223,6 +223,7 @@ STATIC VOID CfgMenuDefault(char * pLine);
 STATIC char * skipwh(char * s);
 STATIC int iswh(unsigned char c);
 STATIC char * scan(char * s, char * d);
+STATIC char * scan_simple(char * s, char * d);
 STATIC BOOL isnum(char ch);
 #if 0
 STATIC COUNT tolower(COUNT c);
@@ -1383,7 +1384,7 @@ char *GetStringArg(char * pLine, char * pszString)
   pLine = skipwh(pLine);
 
   /* just return whatever string is there, including null         */
-  return scan(pLine, pszString);
+  return scan_simple(pLine, pszString);
 }
 
 STATIC void Config_Buffers(char * pLine)
@@ -2246,6 +2247,15 @@ STATIC char * scan(char * s, char * d)
         *d++ = *s;
       s++;
     }
+  *d = '\0';
+  return s;
+}
+
+STATIC char * scan_simple(char * s, char * d)
+{
+  s = skipwh(s);
+  while (*s && !iswh(*s))
+    *d++ = *s++;
   *d = '\0';
   return s;
 }

commit 241a249aeadc6f3590b2d7a48d43a7676e074214

fix SET command broken by d300a36c
SET command has '=' in its arg, which should be parsed properly.

kernel/config.c
@@ -2288,7 +2288,7 @@ STATIC char * scan(char * s, char * d)
 STATIC char * scan_simple(char * s, char * d)
 {
   s = skipwh(s);
-  while (*s && !iswh(*s))
+  while (*s && !iswh(*s) && *s != '=')
     *d++ = *s++;
   *d = '\0';
   return s;