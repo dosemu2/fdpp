ca269700fc2a3d5f7cb212ad78aec1b226537ec0
tkchia <tkchia@users.noreply.github.com>
2022-07-21 16:38:19 +0000

    FCB (int 21h func 29h): should keep parsing name even if drive letter invalid
    Should address https://github.com/dosemu2/fdpp/issues/200 .

--- kernel/fcbfns.c
+++ kernel/fcbfns.c
@@ -89,7 +89,7 @@ UBYTE FAR *FatGetDrvData(UBYTE drive, UBYTE * pspc, UWORD * bps, UWORD * nc)
 #ifndef IPL
 UWORD FcbParseFname(UBYTE *wTestMode, const char FAR * lpFileName, fcb FAR * lpFcb)
 {
-  WORD wRetCodeName = FALSE, wRetCodeExt = FALSE;
+  WORD wRetCodeDrive = FALSE, wRetCodeName = FALSE, wRetCodeExt = FALSE;
 
   /* pjv -- ExtFcbToFcb?                                          */
 
@@ -104,18 +104,20 @@ UWORD FcbParseFname(UBYTE *wTestMode, const char FAR * lpFileName, fcb FAR * lpF
   lpFileName = ParseSkipWh(lpFileName);
 
   /* Now check for drive specification                            */
-  /* If drive specified, set to it (when valid) otherwise         */
+  /* If drive specified, set to it otherwise                      */
   /* set to default drive unless leave as-is requested            */
-  if (*(lpFileName + 1) == ':')
+
+  /* Undocumented behavior: should keep parsing even if drive     */
+  /* specification is invalid  -- tkchia 20220715                 */
+  /* drive specification can refer to an invalid drive, but must  */
+  /* not itself be a file name delimiter!  -- tkchia 20220716     */
+  if (!TestFieldSeps(lpFileName) && *(lpFileName + 1) == ':')
   {
     /* non-portable construct to be changed                 */
     REG UBYTE Drive = DosUpFChar(*lpFileName) - 'A';
 
     if (get_cds(Drive) == NULL)
-    {
-      *wTestMode = PARSE_RET_BADDRIVE;
-      return FP_OFF(lpFileName);
-    }
+      wRetCodeDrive = TRUE;
 
     lpFcb->fcb_drive = Drive + 1;
     lpFileName += 2;
@@ -162,7 +164,12 @@ UWORD FcbParseFname(UBYTE *wTestMode, const char FAR * lpFileName, fcb FAR * lpF
         GetNameField(++lpFileName, lpFcb->fcb_fext,
                      FEXT_SIZE, (BOOL *) & wRetCodeExt);
 
-  *wTestMode = (wRetCodeName | wRetCodeExt) ? PARSE_RET_WILD : PARSE_RET_NOWILD;
+  if (wRetCodeDrive)
+    *wTestMode = PARSE_RET_BADDRIVE;
+  else if (wRetCodeName | wRetCodeExt)
+    *wTestMode = PARSE_RET_WILD;
+  else
+    *wTestMode = PARSE_RET_NOWILD;
   return FP_OFF(lpFileName);
 }
 
