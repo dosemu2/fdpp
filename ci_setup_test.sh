8524ef3f9353ebeaf39928d9f548199315d6e712
Andrew Bird <ajb@spheresystems.co.uk>
2023-11-05 17:58:19 +0000

    CI: test using test suite and dosemu from git

---
+++ ci_setup_test.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "${DIR_TEST_DOSEMU}" ] ; then
+   echo env var "DIR_TEST_DOSEMU" is empty or missing
+   exit 1
+fi
+
+if [ -z "${DIR_INSTALLED_FDPP}" ] ; then
+   echo env var "DIR_INSTALLED_FDPP" is empty or missing
+   exit 1
+fi
+
+test -d ${DIR_TEST_DOSEMU} && (echo $DIR_TEST_DOSEMU already exists && exit 1)
+
+DIR_ROOT="$(pwd)"
+
+# Grab the dosemu2 source
+git clone --depth 1 --no-single-branch https://github.com/dosemu2/dosemu2.git ${DIR_TEST_DOSEMU}
+
+# Install fdpp into local directory
+make install PREFIX="${DIR_ROOT}/${DIR_INSTALLED_FDPP}"
+
+(
+  cd ${DIR_TEST_DOSEMU} || exit 2
+  [ -z "${USE_DOSEMU_BRANCH}" ] || git checkout "${USE_DOSEMU_BRANCH}"
+
+  # Remove fdpp-dev from debian build dependencies (use bash as padding)
+  sed -i -e 's/fdpp-dev,/bash,/' debian/control
+
+  # Use debian package build deps so we don't have to maintain a list here
+  sudo apt install devscripts equivs
+  mk-build-deps --install --root-cmd sudo
+
+  export PKG_CONFIG_PATH=${DIR_ROOT}/${DIR_INSTALLED_FDPP}/lib/pkgconfig
+  CC=clang ./default-configure -d
+  make
+
+  # Install the FAT mount helper
+  sudo cp test/dosemu_fat_mount.sh /bin/.
+  sudo chown root.root /bin/dosemu_fat_mount.sh
+  sudo chmod 755 /bin/dosemu_fat_mount.sh
+
+  # Install the TAP helper
+  sudo cp test/dosemu_tap_interface.sh /bin/.
+  sudo chown root.root /bin/dosemu_tap_interface.sh
+  sudo chmod 755 /bin/dosemu_tap_interface.sh
+
+  # Install the extra packages needed to run the test suite
+  sudo add-apt-repository -y ppa:dosemu2/ppa
+  sudo add-apt-repository -y ppa:jwt27/djgpp-toolchain
+  sudo add-apt-repository -y ppa:tkchia/build-ia16
+  sudo apt update -q
+
+  sudo apt install -y \
+    comcom32 \
+    python3-cpuinfo \
+    python3-pexpect \
+    mtools \
+    gcc-djgpp \
+    djgpp-dev \
+    qemu-system-common \
+    gcc-ia16-elf \
+    libi86-ia16-elf \
+    libi86-testsuite-ia16-elf \
+    nasm \
+    gcc-multilib \
+    dos2unix \
+    bridge-utils \
+    libvirt-daemon \
+    libvirt-daemon-system
+
+  # Prepare the cputests
+  make -C test/cpu clean all
+
+  # Get the test binaries
+  test/test_dos.py --get-test-binaries
+)
