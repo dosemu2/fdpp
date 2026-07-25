96e0f401838d3e01f2a5e45e01825cdbe17dc5b8
Andrew Bird <ajb@spheresystems.co.uk>
2026-07-25 18:24:53 +0100

    CI: Enable the processor tests again
    1/ The processor tests got moved to a separate file, so they
       were no longer being run in this repo.
    2/ The processor tests now make the test binaries themselves.
    3/ Install Watcom

--- ci_test.sh
+++ ci_test.sh
@@ -19,6 +19,20 @@ DIR_ROOT="$(pwd)"
 
   # Now do the tests
   export LD_LIBRARY_PATH=${DIR_ROOT}/${DIR_INSTALLED_FDPP}/lib/fdpp
+  export PYTHONUNBUFFERED=1
   export SKIP_EXPENSIVE=1
+
+  cat >&2 << EOF
+===============================================================
+=      Processor tests run on emulated CPU, KVM and VM86      =
+===============================================================
+EOF
+  env NO_FAILFAST=1 python3 test/test_processor.py
+
+cat >&2 << EOF2
+===============================================================
+=                    Other tests run on KVM                   =
+===============================================================
+EOF2
   test/test_dosemu.py PPDOSGITTestCase
 )
