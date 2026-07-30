8120fa2ab41305ca1b97b5bb57642327db87ee4f
Stas Sergeev <stsp2@yandex.ru>
2026-07-30 22:04:05 +0300

    deb: remove clang and build-essentials
    Instead add clang to ci_prereq.sh as its still used on CI.

--- ci_prereq.sh
+++ ci_prereq.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt install -y \
   devscripts \
   equivs \
-  git
+  git \
+  clang
 
-sudo add-apt-repository ppa:stsp-0/nasm-segelf
 sudo add-apt-repository ppa:stsp-0/thunk-gen
 mk-build-deps --install --root-cmd sudo
